2201100a61578265c174b7ae507cb2d9729f7ce6
NejraBahtic <nejrabahtic@gmail.com>
2019-05-06 23:39:32 +0200

    Created project

---
+++ src/main/java/com/example/ppis/PpisApplication.java
@@ -0,0 +1,13 @@
+package com.example.ppis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpisApplication.class, args);
+	}
+
+}
