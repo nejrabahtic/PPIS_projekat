81ffdceca7b65dc62076a9448112bcc297f57b06
Muhamed Delalic <muha.delalic@gmail.com>
2019-05-23 07:25:41 +0200

    work on form

--- FE/src/components/Report.js
+++ FE/src/components/Report.js
@@ -1,11 +1,87 @@
 import React, { Component } from 'react';
+import { Button, Checkbox, Form, Header } from 'semantic-ui-react'
 
+import "../styles/Report.css";
+import "../styles/Color.css"
+let userTypes = {
+    USER: 0,
+    DEVELOPER: 1,
+    ADMINISTRATOR: 2
+}
 
 export default class Report extends Component {
 
+
+    constructor(props){
+        super(props);
+        this.state = {
+            activeView: userTypes.USER,
+            type: null
+        }
+    }
+
+    handleTypeChange = (event, data) => {
+        this.setState({type: data.value});
+    }
     render(){
+        const { activeView, type } = this.state;
         return (
-            <div> Request and Incident Form goes here.</div>
+            <div className="report-page"> 
+                <Button.Group className="report-buttons">
+                    <Button 
+                        onClick={() => this.setState({activeView: userTypes.USER })} 
+                        active={activeView === userTypes.USER}>
+                        User
+                    </Button>
+                    <Button 
+                        onClick={() => this.setState({activeView: userTypes.DEVELOPER })}
+                        active={activeView === userTypes.DEVELOPER}>
+                        Developer
+                    </Button>
+                    <Button 
+                        onClick={() => this.setState({activeView: userTypes.ADMINISTRATOR })}
+                        active={activeView === userTypes.ADMINISTRATOR}
+                        >
+                        Administrator
+                    </Button>
+                </Button.Group>
+                <Form className="report-form secondary-color-bg">
+                    <Header className="accent-color">Request/Incident</Header>
+                        <Form.Field>
+                            <label className="empty-color">Title:</label>
+                            <input></input>
+                        </Form.Field>
+                        <Form.Field>
+                            <label className="empty-color">Description:</label>
+                            <textarea className="description" rows="10" ></textarea>
+                        </Form.Field>
+                        <Form.Field>
+                            <label className="empty-color">Type:</label>
+                            <Form.Radio
+                                label='Request'
+                                value='req'
+                                checked={type === 'req'}
+                                onChange={this.handleTypeChange}
+                                
+                            />
+                            <Form.Radio
+                                label='Incident'    
+                                value='inc'
+                                checked={type === 'inc'}
+                                onChange={this.handleTypeChange}
+                            />
+                            <Form.Radio
+                                label='Not Sure'
+                                value='nots'
+                                disabled={activeView !== userTypes.USER}
+                                checked={type === 'nots'}
+                                onChange={this.handleTypeChange}
+                            />
+                        </Form.Field>
+                    <Button type="sumbit"className="sumbit-button">Submit</Button>
+                </Form>
+
+            </div>
         );
     }
 };
