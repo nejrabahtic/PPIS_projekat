777397b7cdb691e34b6bee99c57b7f7f1da863ab
Muhamed Delalic <muha.delalic@gmail.com>
2019-06-09 14:59:31 +0200

    added localStorage storing and different roles

--- FE/src/services/Auth.js
+++ FE/src/services/Auth.js
@@ -1,29 +1,67 @@
-var loggedId = null;
-var events = null;
+const ID = "ID";
+const USERNAME = "USERNAME";
+const ROLE = "ROLE";
+
+var events = {};
+
+var callEvents = () => {
+    Object.values(events).map(f => f());
+}
 
 export default {
     getId: () => {
-        return loggedId;
+        return localStorage.getItem(ID);
     },
     isIdSet: () => {
-        return loggedId !== null;
+        return localStorage.getItem(ID) !== null;
     },
     setId: (id) => {
-        loggedId = id; 
-        
-        if (events !== null){
-            events();
-        }
+        localStorage.setItem(ID, id); 
+        callEvents();
     },
     removeId: () => {
-        loggedId = null;
-        
-        if (events !== null){
-            events();
-        }
+        localStorage.removeItem(ID);
+        callEvents();
     },
-    subscribeToUpdate: (f) => {
-        events = f;
-    }
+    setRole: (role) => {
+        localStorage.setItem(ROLE, role);
+        callEvents();
+    },
+    removeRole: () => {
+        localStorage.removeItem(ROLE)
+        callEvents();
+    },
+    getRole: () => {
+        return localStorage.getItem(ROLE);
+    },
+    setUsername: (username) => {
+        localStorage.setItem(USERNAME, username);
+        callEvents();
+    },
+    removeUsername: () => {
+        localStorage.removeItem(USERNAME);
+        callEvents();
+    },
+    getUsername: () => {
+        return localStorage.getItem(USERNAME);
+    },
+    clear: () => {
+        localStorage.removeItem(USERNAME);
+        localStorage.removeItem(ROLE);
+        localStorage.removeItem(ID);
+        callEvents();
 
+    },
+    subscribeToUpdate: (eventName, f) => {
+        events[eventName] = f;
+    },
+    unsubscribeToUpdate: (eventName) => {
+        if(events[eventName])
+            delete events[eventName];
+    },
+    isLoggedIn(){
+        return localStorage.getItem(ID) !== null &&
+               localStorage.getItem(ROLE) !== null &&
+               localStorage.getItem(USERNAME) !== null;
+    }
 }
