a59499e13c473f6e66977a776a827d77120c0f9d
aminaaljicevic <aaljicevic1@etf.unsa.ba>
2019-06-11 09:21:10 +0200

    Added arrays and changed Preview of requests

--- FE/src/views/DetailedRequest.js
+++ FE/src/views/DetailedRequest.js
@@ -1,67 +1,93 @@
 import React, { Component } from 'react';
 
 export default class DetailedRequest extends Component {
+  constructor(props){
+      super(props);
+      this.state = {
+        requests:  [{
+          'id': '1',
+          'title': 'PRvi request',
+          'date': '02/09/2019',
+          'short_text': 'NEki tekst koji je kraci ya prvi request tekst koji je kraci jhzhzub tekst koji je kraci jhzhzub tekst koji je kraci jhzhzub',
+          'department': 'IT department',
+          'creator': 'Neko Nekic',
+          'priority': 'high',
+          'urgency':'high',
+          'status':'created',
+          'report_method': 'By application',
+          'contact_method': 'Telephone',
+          'long_text': 'A long description A long description A long description A long DescriptionA short descriptionA long description A long description A long description A long description A long DescriptionA short descriptionA long description A long description A long description A long description A long DescriptionA short descriptionA long description A long description A long description A long description A long DescriptionA short descriptionA long description A long description A long description A long description A long DescriptionA short descriptionA long description'
+        }]
+
+      }
+}
     render(){
         return(
+<div>
+{this.state.requests.map(item => (
+    <div class="ui computer reversed equal width grid" key={item}>
 
-          <div class="ui computer reversed equal width grid">
+
             <div class="row">
 
               <div class="column"></div>
               <div class="column"></div>
-              <div class="column">
-                  <h1> Naziv requesta </h1>
+              <div class="column"></div>
+              <div class="column"></div>
+              <div class="column"></div>
+
+              <div class="left floated left aligned column">
+                  <h1 class="naslov"> {item.title}</h1>
                                     <h3>Type: Request</h3>
-                  <span class="time">09/06/2019</span>
-
-
+                  <span class="time">{item.date}</span>
               </div>
             </div>
-
-
-
 
 
           <div class="row">
               <div class="column"></div>
               <div class="column"></div>
             <div class="left floated left aligned column">
-            <br></br><br></br>
                   <div role="list" class="ui selection middle aligned list">
+                  <p></p>
                       <div role="listitem" class="item">
                       <i aria-hidden="true" class="users icon"></i>
-                        <div class="content">Management department</div>
-                      </div><br></br>
+                        <div class="content">{item.department}</div>
+                      </div>  <p></p>
                       <div role="listitem" class="item">
                       <i aria-hidden="true" class="user icon"></i>
-                        <div class="content">Neko Nekic</div>
+                        <div class="content">{item.creator}</div>
                       </div>
                   </div>
             </div>
 
 
             <div class="right floated right aligned column">
-            <br></br><br></br>
+            <p></p>
                     <h4 class="meta">Assigned to:</h4>
                     <h4 class="meta">Created by:</h4><br></br>
                 </div>
-
-              <div class="column"></div>
-              <div class="column">
-                <h5>High </h5>
-                <h5>Medium </h5>
-                <h5>Created </h5>
+                <div class="column">
+                <p></p>
                 <h5>By application </h5>
                 <h5>Telephone </h5>
+                </div>
+              <div class="right floated right aligned column">
+              <p></p>
+              <h5>Report method: </h5>
+              <h5>Contact method: </h5>
+              </div>
+              <div class="column">
+                <h5>{this.priority} </h5>
+                <h5>{this.urgency} </h5>
+                <h5>{this.status} </h5>
               </div>
               <div class="right floated right aligned column">
                   <h5>Priority: </h5>
                   <h5>Urgency: </h5>
                   <h5>Status: </h5>
-                  <h5>Report method: </h5>
-                  <h5>Contact method: </h5>
               </div>
-            </div>
+        </div>
 
 
             <div class="row">
@@ -82,8 +108,8 @@ export default class DetailedRequest extends Component {
 
 
             <div class="row">
-              <div class="column">A long description A long description A long description A long DescriptionA short descriptionA long description A long description A long description A long description A long DescriptionA short descriptionA long description A long description A long description A long description A long DescriptionA short descriptionA long description A long description A long description A long description A long DescriptionA short descriptionA long description A long description A long description A long description A long DescriptionA short descriptionA long description</div>
-              <div class="column"><h5>A short description A short description A short description A short DescriptionA short descriptionA short description</h5></div>
+              <div class="column">{item.long_text}</div>
+              <div class="column"><h5>{item.short_text}</h5></div>
             </div>
 
 
@@ -128,13 +154,9 @@ export default class DetailedRequest extends Component {
           </div>
 
 
-
-
-
-
     </div>
-
-
+))}
+</div>
 
 
         )
